4d986e93e4ef6522ecedd5657a6a2da8692f2593
Greg McMahan <53842892+gmcmacran@users.noreply.github.com>
2021-12-08 17:22:50 -0600

    delete rand

--- tests/testthat/test_4_fold_logical_functions.R
+++ tests/testthat/test_4_fold_logical_functions.R
@@ -18,10 +18,9 @@ test_that("Test any over empty vector and single values", {
   expect_true(recursive_any(FALSE) == any(FALSE))
 })
 
-set.seed(1)
-rand <- runif(10) > .5
 test_that("Test any over vectors.", {
   expect_true(recursive_any(rep(TRUE, 10)) == any(rep(TRUE, 10)))
   expect_true(recursive_any(rep(FALSE, 10)) == any(rep(FALSE, 10)))
   expect_true(recursive_any(rand) == any(rand))
 })
+rm(rand)
--- tests/testthat/test_6_cumulative_logical_functions.R
+++ tests/testthat/test_6_cumulative_logical_functions.R
@@ -18,10 +18,9 @@ test_that("Test any over empty vector and single values", {
   expect_true(cumulative_any(FALSE) == lest::cumany(FALSE))
 })
 
-set.seed(1)
-rand <- runif(10) > .5
 test_that("Test any over vectors.", {
   expect_true(all(cumulative_any(rep(TRUE, 10)) == lest::cumany(rep(TRUE, 10))))
   expect_true(all(cumulative_any(rep(FALSE, 10)) == lest::cumany(rep(FALSE, 10))))
   expect_true(all(cumulative_any(rand) == lest::cumany(rand)))
 })
+rm(rand)
