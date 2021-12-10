e91000811b03248688ec50f58242855bae202e0f
Greg McMahan <53842892+gmcmacran@users.noreply.github.com>
2021-12-10 08:25:47 -0600

    Add ackermann with tests

--- R/9_basic_recursive_functions.R
+++ R/9_basic_recursive_functions.R
@@ -19,3 +19,30 @@ recursive_reverse <- function(x) {
     return(c(x[last], recursive_reverse(x[-last])))
   }
 }
+
+#' Standard functions implemented by recursion
+#'
+#' @param m A positive integer.
+#' @param n A positive integer.
+#' @details
+#'
+#' Functions leveraging recursion.
+#'
+#' This function will only finish in a reasonable amount of time
+#' for m and n less than or equal to 4.
+#'
+#' @examples
+#' library(functionalPlayground)
+#'
+#' ackermann(2, 1)
+#' ackermann(1, 2)
+#' @export
+ackermann <- function(m, n) {
+  if (m == 0) {
+    return(n + 1)
+  } else if (n == 0) {
+    return(ackermann(m - 1, 1))
+  } else {
+    return(ackermann(m - 1, ackermann(m, n - 1)))
+  }
+}
